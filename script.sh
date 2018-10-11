89b1d9d442713bfe3ead9736573d571212be664f
Ashish <ashish@numinolabs.com>
2018-10-11 19:10:04 +0530

    updated script

--- script.sh
+++ script.sh
@@ -1,6 +1,6 @@
 NEWPROPERTIES=$(diff -y one.ini two.ini | tr -d '[:blank:]' | cat -n | grep '[<]' | sed 's/<$//' | sed 's/ //g')
 while read -r line ; do
     lineno=$(echo $line | awk '{print $1}')
-    val=$(echo $line | awk '{print $2}')
-    ex -s -c "${lineno}i|$val" -c x two.ini
+    prop=$(echo $line | awk '{print $2}')
+    ex -s -c "${lineno}i|$prop" -c x two.ini
 done <<< "$NEWPROPERTIES"
